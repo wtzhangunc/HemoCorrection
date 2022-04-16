104da2bb79d4c841da55e20d071466f1205870aa
wtzhangunc <70246400+wtzhangunc@users.noreply.github.com>
2022-04-16 16:28:50 -0400

    Update GreenSignalsCorrectedbyHb.m

--- GreenSignalsCorrectedbyHb.m
+++ GreenSignalsCorrectedbyHb.m
@@ -3,21 +3,21 @@ function [Green_cor, Green_uncor_perc]=GreenSignalsCorrectionbyHb_matrix(Green_u
 % Green_uncor should be 10 time points longer than HbO and HbR
 % Weiting Zhang, 04/07/2022
 
-parameters = xlsread('~/parameters_green4correction.xlsx');
+parameters = xlsread('./parameters_green4correction.xlsx');
 
 OxyE488 = 24174.8;
 Deoxy488 = 15898;
 X488 = 0.0451; % excitation laser for tdTomato. use X561 if excited by 561 nm laser
-
 x=((OxyE488 * X488 + parameters(:,2)) .* parameters(:,4))';
 y=((Deoxy488 * X488 + parameters(:,3)) .* parameters(:,4))';
 
+% use following three lines to replace lines 8-12 if the green signal is excited by 561 nm laser
 %X561 = 0.0411;OxyE561 = 32617;Deoxy561 = 53032;% X value is from 29 trials
 %x=((OxyE561 * X561 + parameters(:,2)) .* parameters(:,4))';
 %y=((Deoxy561 * X561 + parameters(:,3)) .* parameters(:,4))';
 
 HbO = sgolayfilt(HbO,2,61)*1.2; %1.2 is the scaling factor which may vary
-HbR = sgolayfilt(HbR,2,61)*1.2;
+HbR = sgolayfilt(HbR,2,61)*1.2; %1.2 is the scaling factor which may vary
 c = HbO * x + HbR * y ;
 
 Green_uncorrected = Green_uncor(11:end)./mean(Green_uncor(1:10));
